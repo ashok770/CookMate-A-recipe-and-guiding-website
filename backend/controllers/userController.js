b8d1b3102f7a722a8a1f8c1d6761e784a37f3518
ashok770 <ashoktamata6002@gmail.com>
2025-12-16 19:19:04 +0000

    JWT tested in the postman it works

---
+++ backend/controllers/userController.js
@@ -0,0 +1,10 @@
+exports.getUserProfile = async (req, res) => {
+  try {
+    res.status(200).json({
+      success: true,
+      user: req.user,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const userRoutes = require("./routes/userRoutes");
 
 const connectDB = require("./config/db");
 
@@ -15,6 +16,7 @@ app.use(express.json());
 
 // Routes
 app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/users", userRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
